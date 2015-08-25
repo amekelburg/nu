a526569852376ccb17b899ebf6de8f762ee2661a
Aleksey Gureiev <spyromus@noizeramp.com>
2015-08-25 21:27:21 +0300

    returned old localization scheme back

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Deter
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     config.cache_store = :redis_store, 'redis://localhost:6379/0/deter-cache', { expires_in: 90.minutes }
