cf77df7da89baa1d357c313b4bc3d74539c4e76e
Aleksey Gureiev <spyromus@noizeramp.com>
2015-09-04 22:29:26 +0300

    a

--- config/deploy/deter-alt.rb
+++
@@ -1,9 +0,0 @@
-role :app, %w{alg@noizeramp.com}
-role :web, %w{alg@noizeramp.com}
-role :db,  %w{alg@noizeramp.com}
-
-set :deploy_to, '/home/alg/sites/deter-alt.noizeramp.com'
-set :rails_env, 'production'
-set :branch,    'deter-alt'
-
-set :rvm_ruby_version, '2.1.3'
