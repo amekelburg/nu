c37efa299d7c100e92199a6b15b706aa84322c28
Aleksey Gureiev <spyromus@noizeramp.com>
2015-09-10 17:47:51 +0300

    don't let users add experiments if they have no rights

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,4 +1,15 @@
 class Library < Struct.new(:id, :owner, :perms, :experiments, :members)
-  MAIN_LIBRARY_ID   = AppConfig['main_library_id']
-  MAIN_LIBRARY_NAME = 'Main Library'
+
+  ADD_EXPERIMENT        = "ADD_EXPERIMENT"
+  REMOVE_EXPERIMENT     = "REMOVE_EXPERIMENT"
+  READ_LIBRARY          = "READ_LIBRARY"
+  MODIFY_LIBRARY_ACCESS = "MODIFY_LIBRARY_ACCESS"
+
+  MAIN_LIBRARY_ID       = AppConfig['main_library_id']
+  MAIN_LIBRARY_NAME     = 'Main Library'
+
+  def can_add_experiment?(uid)
+    [ self.perms ].flatten.include?(ADD_EXPERIMENT)
+  end
+
 end
